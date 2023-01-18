3b9f91e09bcfabda6c9543fddbf465c8f37ba21e
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-01-18 06:55:58 -0600

    rename

--- app/public/views.py
+++ app/public/views.py
@@ -6,6 +6,6 @@ def index(request):
     if request.user.is_authenticated:
         return redirect(to=settings.LOGIN_REDIRECT_URL)
     context = dict(
-        title="Eve Honcho",
+        title="Market Monitor",
     )
     return render(request=request, template_name="public/index.html", context=context)
